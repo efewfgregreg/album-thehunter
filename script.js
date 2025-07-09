b517f9cfca1b46a7f9df6e63ddf801acb8ba4e12
efewfgregreg <marcos123vinicius1419@gmail.com>
2025-07-09 20:51:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,13 @@
 // ======== INICIALIZAÇÃO DO FIREBASE (COM SEUS DADOS) ========
 // =================================================================
 const firebaseConfig = {
-  apiKey: "AIzaSyD_vgZDTseipBQgo2oXJezUyczCEzWg_8w",
+  apiKey: "AIzaSyD_vgZDTseipBQgo2oXJeZUyczCEzWg_8w",
   authDomain: "album-thehunter.firebaseapp.com",
   projectId: "album-thehunter",
-  storageBucket: "album-thehunter.appspot.com",
+  storageBucket: "album-thehunter.firebasestorage.app",
   messagingSenderId: "369481100721",
-  appId: "1:369481100721:web:e5ce08c635536fb7e0a190"
+  appId: "1:369481100721:web:e5ce08c635536fb7e0a190",
+  measurementId: "G-3G5VBWBEDL"
 };
 
 // Inicializa os serviços do Firebase que vamos usar
