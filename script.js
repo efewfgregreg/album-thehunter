b5cdaeb6ebbb40b89a0e74eaf7fafc126fcd1e4e
efewfgregreg <marcos123vinicius1419@gmail.com>
2025-07-10 23:50:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,21 +5,13 @@
 // =================================================================
 
 const firebaseConfig = {
-
-  apiKey: "AIzaSyD_vgZDTseipBQgo2oXJeZUyczCEzWg_8w",
-
-  authDomain: "album-thehunter.firebaseapp.com",
-
-  projectId: "album-thehunter",
-
-  storageBucket: "album-thehunter.firebasestorage.app",
-
-  messagingSenderId: "369481100721",
-
-  appId: "1:369481100721:web:e5ce08c635536fb7e0a190",
-
-  measurementId: "G-3G5VBWBEDL"
-
+  apiKey: "AIzaSyD_vgZDTseipBQgo2oXJeZUyczCEzWg_8w",
+  authDomain: "album-thehunter.firebaseapp.com",
+  projectId: "album-thehunter",
+  storageBucket: "album-thehunter.firebasestorage.app",
+  messagingSenderId: "369481100721",
+  appId: "1:369481100721:web:e5ce08c635536fb7e0a190",
+  measurementId: "G-3G5VBWBEDL"
 };
 
 
