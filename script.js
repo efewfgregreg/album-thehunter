65c99af4af3c0859e02a5101fd3017a85d60db6f
efewfgregreg <marcos123vinicius1419@gmail.com>
2025-07-23 22:54:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,15 @@
 // ======== INICIALIZAÇÃO DO FIREBASE (COM SEUS DADOS) ========
 // =================================================================
 const firebaseConfig = {
-  apiKey: "AIzaSyD_vgZDTseipBQgo2oXJezUyczCEzWg_8w",
-  authDomain: "album-thehunter.firebaseapp.com",
-  projectId: "album-thehunter",
-  storageBucket: "album-thehunter.firebasestorage.app",
-  messagingSenderId: "369481100721",
-  appId: "1:369481100721:web:e5ce08c635536fb7e0a190"
+    apiKey: "AIzaSyD_vgZDTseipBQgo2oXJeZUyczCEzWg_8w",
+    authDomain: "album-thehunter.firebaseapp.com",
+    projectId: "album-thehunter",
+    storageBucket: "album-thehunter.firebasestorage.app",
+    messageSenderId: "369481100721",
+    appId: "1:369481100721:web:e5ce08c635536fb7e0a190",
+    measurementId: "G-3G5VBWBEDL"
 };
+
 
 // Inicializa os serviços do Firebase que vamos usar
 const app = firebase.initializeApp(firebaseConfig);
